ebf32f75df46eceaa14b490368ad48cfc63adfac
Jakob Voss <voss@gbv.de>
2019-02-07 17:51:01 +0100

    Rename example file

--- test/extract-citekeys.js
+++ test/extract-citekeys.js
@@ -5,7 +5,7 @@ const { extractCitekeys } = require('../lib/pwcite')
 describe('extract citekeys', () => {
   let examples = {
     'minimal': { Q18507561: 'Q18507561' },
-    'example-1': { Vrand04: 'Q18507561' },
+    'example': { Vrand04: 'Q18507561' },
     'nocite': { Q55239420: 'Q55239420', foo: '' }
   }
 
