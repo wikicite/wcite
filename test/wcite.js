cb58aabe619396363a5d2023ad820e07badde5fc
Jakob Voss <voss@gbv.de>
2019-02-09 09:41:39 +0100

    Add tests

--- test/wcite.js
+++ test/wcite.js
@@ -2,6 +2,14 @@
 const { should, pkgfile, outstream } = require('./util')
 const { wcite } = require('../index')
 
+const example = file => pkgfile(`examples/${file}`)
+const output = (opts, cmd, ...args) => {
+  opts.out = outstream()
+  wcite(opts)[cmd](...args)
+  return should(opts.out.data)
+}
+
+
 describe('wcite', () => {
   it('reads document and/or bibliography', () => {
     let document = pkgfile('home/wcite.md')
@@ -11,11 +19,24 @@ describe('wcite', () => {
     should.deepEqual(cite1.bibliography.ids(), cite2.bibliography.ids())
   })
 
-  it('lists ids', () => {
-    let bibliography = pkgfile('examples/nocite-refs.json')
-    let out = outstream()
-    let cite = wcite({ bibliography, out })
-    cite.list()
-    should(out.data).equal('Q55239420\n')
+  it('lists ids from bibliography file', () => {
+    let bibliography = example('nocite-refs.json')
+    output({ bibliography }, 'list').equal('Q55239420\n') 
+  })
+
+  it('lists ids and aliases from document', () => {
+    let document = example('example.md')
+    output({ document }, 'list').equal('Vrand04: Q18507561\n')
+  })
+
+/*  it('lists ids and aliases from document and bibliography', () => {
+    let bibliography = example('nocite-refs.json')
+    let document = example('citeall.yaml')
+    output({ document, bibliography }, 'list').equal('Vrand04: Q18507561\nQ55239420\n')
+  })
+*/
+
+  it('complains on missing YAML header', () => {
+    should(() => { wcite({ document: example('minimal.md') }) }).throw()
   })
 })
