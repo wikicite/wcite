6387c1fd692f055b6c26c1bbf4edb5aa5d2709dc
Jakob Voss <voss@gbv.de>
2019-02-20 17:59:09 +0100

    Add test for wcite.add

--- test/wcite.js
+++ test/wcite.js
@@ -5,11 +5,14 @@ const { wcite } = require('../index')
 const example = file => pkgfile(`examples/${file}`)
 const output = (opts, cmd, ...args) => {
   opts.out = outstream()
+  opts.err = opts.out
   wcite(opts)[cmd](...args)
   return should(opts.out.data)
 }
 
 describe('wcite', () => {
+  // constructor
+
   it('reads document and/or bibliography', () => {
     let document = pkgfile('home/wcite.md')
     let bibliography = pkgfile('home/references.json')
@@ -17,6 +20,12 @@ describe('wcite', () => {
     let cite2 = wcite({ bibliography })
     should.deepEqual(cite1.doc.bibliography.ids(), cite2.doc.bibliography.ids())
   })
+
+  it('complains on missing YAML header', () => {
+    should(() => { wcite({ document: example('minimal.md') }) }).throw()
+  })
+
+  // list
 
   it('lists ids from bibliography file', () => {
     let bibliography = example('nocite-refs.json')
@@ -34,7 +43,16 @@ describe('wcite', () => {
     output({ document, bibliography }, 'list').equal('Vrand04: Q18507561\nQ2013\nQ55239420\n')
   })
 
-  it('complains on missing YAML header', () => {
-    should(() => { wcite({ document: example('minimal.md') }) }).throw()
+  // add
+
+  it('does not add existing item', () => {
+    let bibliography = example('nocite-refs.json')
+    output({ bibliography }, 'add', ['Q55239420']).equal('Q55239420 already added\n')
+  })
+
+  it('supports aliases to add items', () => {
+    let bibliography = example('nocite-refs.json')
+    let citekeys = { Frankenstein: 'Q55239420' }
+    output({ bibliography, citekeys }, 'add', ['Frankenstein']).equal('Frankenstein already added\n')
   })
 })
